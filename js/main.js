9a8fd8d2a3fff0efcba85cca5695e1ee9dd5d326
Wesley Hartford <wesley@ze.com>
2020-02-13 08:02:49 -0800

    Initial check in of basic working tool

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function buildOutput() {
+  let template = document.getElementById("template-input").value;
+  let placeholder = template.match(/^.*?(\d+).*$/)[1];
+  let repeat = document.getElementById("repeat-input").value;
+  let content = "";
+  for (let n = 0; n < repeat; n++) {
+    let padding = new Array(placeholder.length + 1).join('0');
+    let replacement = (padding + (n + 1)).slice(-padding.length);
+    content += template.replace(new RegExp(placeholder, 'g'), replacement) + "\n"
+  }
+  let area = document.getElementById("output-area");
+  area.value = content
+}
+
+function clearOutput() {
+  document.getElementById("output-area").value = ""
+}
+
+function copyOutput() {
+  let element = document.getElementById("output-area")
+  element.focus()
+  element.setSelectionRange(0, element.value.length)
+  document.execCommand("copy")
+}
+
+function copyToTemplate(element) {
+  document.getElementById("template-input").value = element.innerHTML
+}
